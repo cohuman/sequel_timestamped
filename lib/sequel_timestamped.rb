9ab9b790b5d53bdd7bdfa8b4c1b961aecfe23a16
Piotr Usewicz <piotr@layer22.com>
2009-01-08 14:12:44 +0000

    Added ability to set custom created_at and updated_at values

--- lib/sequel_timestamped.rb
+++ lib/sequel_timestamped.rb
@@ -12,11 +12,27 @@ module Sequel
       module InstanceMethods
         # Define methods that will be instance-specific here.
         def set_created_at
-          self.created_at = Time.now if self.columns.include?(:created_at)
+          if self.columns.include?(:created_at)
+            self.created_at = Time.now unless self.created_at
+          end
         end
         
         def set_updated_at
-          self.updated_at = Time.now if self.columns.include?(:updated_at)
+          if self.columns.include?(:updated_at)
+            if self.updated_at.nil?
+              self.updated_at = Time.now
+            else
+              if self.new?
+                self.updated_at = values[:updated_at] || Time.now
+              else
+                self.updated_at = if self.changed_columns.include?(:updated_at)
+                  values[:updated_at]
+                else
+                  Time.now
+                end
+              end
+            end
+          end
         end
       end
     end
--- spec/sequel_timestamped_spec.rb
+++ spec/sequel_timestamped_spec.rb
@@ -1,9 +1,8 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
 
 DB = Sequel.sqlite
- 
 
-class TimestampedModel < Sequel::Model(:items)
+class TimestampedModel < Sequel::Model(:items_timestamped)
   set_schema do
     primary_key :id
     varchar :name
@@ -14,7 +13,7 @@ class TimestampedModel < Sequel::Model(:items)
   is :timestamped
 end
 
-class NonTimestampedModel < Sequel::Model(:items)
+class NonTimestampedModel < Sequel::Model(:items_nontimestamped)
   set_schema do
     primary_key :id
     varchar :name
@@ -24,9 +23,9 @@ class NonTimestampedModel < Sequel::Model(:items)
 end
 
 describe Sequel::Plugins::Timestamped, "#created_at" do
-
   before(:each) do
     TimestampedModel.create_table!
+    NonTimestampedModel.create_table!
   end
   
   it "should handle a model without a created_at field" do
@@ -37,16 +36,23 @@ describe Sequel::Plugins::Timestamped, "#created_at" do
     TimestampedModel.create.created_at.should_not be_nil
   end
   
+  it "should allow to specify custom created_at on INSERT" do
+    time_now = Time.now + 3600
+    TimestampedModel.create(:created_at => time_now).created_at.to_i.should eql(time_now.to_i)
+  end
+  
+  it "should allow to update created_at" do
+    updated_time = Time.now - 1800
+    m = TimestampedModel.create
+    m.update(:created_at => updated_time).created_at.to_i.should == updated_time.to_i
+  end
+end
 
+describe Sequel::Plugins::Timestamped, "#updated_at" do
   it "should set updated_at to Time.now on INSERT" do
     TimestampedModel.create.updated_at.should_not be_nil
   end
   
-
-end
-
-describe Sequel::Plugins::Timestamped, "#updated_at" do
-
   it "should set updated_at to Time.now upon UPDATE" do
     t = TimestampedModel.create
     t.updated_at = nil
@@ -54,6 +60,14 @@ describe Sequel::Plugins::Timestamped, "#updated_at" do
     t.updated_at.should_not be_nil
   end
   
+  it "should update updated_at on UPDATE" do
+    t = TimestampedModel.create
+    updated_time = Time.now - 3600
+    Time.stub!(:now).and_return(updated_time)
+    t.update(:name => 'Oh hai!')
+    t.updated_at.to_i.should == updated_time.to_i
+  end
+    
   it "should handle a model without an updated_at field" do
     nt = NonTimestampedModel.create
     nt.name = "hi"
@@ -61,4 +75,14 @@ describe Sequel::Plugins::Timestamped, "#updated_at" do
     nt.should be_valid
   end
 
+  it "should allow to specify custom updated_at on INSERT" do
+    time_now = Time.now + 3600
+    TimestampedModel.create(:updated_at => time_now).updated_at.to_i.should eql(time_now.to_i)
+  end
+  
+  it "should allow to update updated_at column" do
+    updated_time = Time.now - 1800
+    m = TimestampedModel.create
+    m.update(:updated_at => updated_time).updated_at.to_i.should == updated_time.to_i
+  end
 end
